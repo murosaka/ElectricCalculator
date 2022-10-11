1a6e4503a9db886b9db7cf11e62cde862314299c
murosaka <murosaka@gmail.com>
2022-10-11 15:54:36 +0300

    Made functions, added them to eventListeners

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const impedanceInput = document.querySelector("#impedance");
 const resistanceInput = document.querySelector("#resistance");
 
 let voltage = 0.4;
-let phases ="3 ph"; // TODO
 let constant = "Voltage"; // TODO
 let activePower = 0.00;
 let reactivePower = 0.00;
@@ -22,43 +21,127 @@ let current = 0.00;
 let powerFactor = 0.85;
 let efficiency = 1.00;
 let k;
+let impedance = 0.00;
+let reactance = 0.00;
+let resistance = 0.00;
 
-if (phases == "3 ph" || phases == "2 ph") {
-    k = 1.73
-} else {k = 1};
+// -----------------------------------------------------------------------------
+// All calculations as functions
+
+function getK() {
+    if (phasesSelect.value == "threePhases") {
+        k = 1.73;
+    } else if (phasesSelect.value == "twoPhases") {
+        k = 1.73;
+    } else if (phasesSelect.value == "singlePhase") {
+        k = 1
+    };
+    console.log(k);
+};
+
+function getApparentPower() {
+    apparentPower = (activePower / powerFactor).toFixed(2);
+};
+
+function getActivePower() {
+    activePower = (reactivePower / Math.tan(Math.acos(powerFactor))).toFixed(2);
+};
+
+function getReactivePower() {
+    reactivePower = ((apparentPower**2 - activePower**2)**0.5).toFixed(2);
+};
+
+function getCurrent() {
+    current = (apparentPower/(k * voltage * efficiency)).toFixed(2);
+};
+
+function getResistance() {
+    resistance = ((voltage * 1000)/ (current * k)).toFixed(6);
+};
+
+function getImpedance() {
+    impedance = (resistance * powerFactor).toFixed(6);
+};
+
+function getReactance() {
+    reactance = ((resistance**2 - impedance**2)**0.5).toFixed(6);
+};
+// -----------------------------------------------------------------------------
+
+phasesSelect.addEventListener('change', function() {
+    getK();
+    getCurrent();
+    getResistance();
+    getImpedance();
+    getReactance();
+    updateInputs();
+});
 
 voltageInput.addEventListener('change', function(){
     voltage = this.value;
-    if (voltage == 0) {voltage = 0.4};
-    // apparentPower = (activePower / powerFactor).toFixed(2);
-    // apparentPowerInput.value = apparentPower;
-    // reactivePower = ((apparentPower**2 - activePower**2)**0.5).toFixed(2);
-    // reactivePowerInput.value = reactivePower;
-    current = (apparentPower/(k * voltage * efficiency)).toFixed(2);    
-    currentInput.value = current;
+    if (voltage == 0) {voltage = 0.4}; 
+    getK();
+    getApparentPower();
+    getReactivePower();
+    getCurrent();
+    getResistance();
+    getImpedance();
+    getReactance();
+    updateInputs();
 });
 
 activePowerInput.addEventListener('change', function(){
     activePower = this.value;
-    apparentPower = (activePower / powerFactor).toFixed(2);
-    reactivePower = ((apparentPower**2 - activePower**2)**0.5).toFixed(2);
-    current = (apparentPower/(k * voltage * efficiency)).toFixed(2);    
+    getK();
+    getApparentPower();
+    getReactivePower();
+    getCurrent();
+    getResistance();
+    getImpedance();
+    getReactance();
     updateInputs();
 });
 
 reactivePowerInput.addEventListener('change', function(){
     reactivePower = this.value;
-    activePower = (reactivePower / Math.tan(Math.acos(powerFactor))).toFixed(2);
-    apparentPower = (activePower / powerFactor).toFixed(2);
-    current = (apparentPower/(k * voltage * efficiency)).toFixed(2);  
+    getK();
+    getActivePower();
+    getApparentPower();
+    getCurrent();
+    getResistance();
+    getImpedance();
+    getReactance();
     updateInputs();
 });
 
 apparentPowerInput.addEventListener('change', function(){
     apparentPower = this.value;
-    activePower = (apparentPower * powerFactor).toFixed(2);
-    reactivePower = ((apparentPower**2 - activePower**2)**0.5).toFixed(2);
-    current = (apparentPower/(k * voltage * efficiency)).toFixed(2);    
+    getK();
+    getActivePower();
+    getReactivePower();
+    getCurrent();
+    getResistance();
+    getImpedance();``
+    getReactance();
+    updateInputs();
+});
+
+powerFactorInput.addEventListener('change', function(){
+    powerFactor = this.value; 
+    getK();   
+    getReactivePower();
+    getApparentPower();
+    getCurrent();
+    getResistance();
+    getImpedance();
+    getReactance();
+    updateInputs();
+});
+
+efficiencyInput.addEventListener('change', function(){
+    efficiency = this.value;
+    getK();
+    getCurrent();
     updateInputs();
 });
 
@@ -67,6 +150,9 @@ function updateInputs() {
     apparentPowerInput.value = apparentPower;
     reactivePowerInput.value = reactivePower;
     currentInput.value = current;
+    impedanceInput.value = impedance;
+    reactanceInput.value = reactance;
+    resistanceInput.value = resistance;
 }
 
  
